793e4214c3bd573e07417bf97070bda22293850e
Aaron Lun <alun@wehi.edu.au>
2017-11-18 14:33:13 +0000

    Switched extractPatch to use new internals.

--- R/extractPatch.R
+++ R/extractPatch.R
@@ -4,50 +4,57 @@ extractPatch <- function(file, param, first.region, second.region=first.region, 
 #
 # written by Aaron Lun
 # created 13 May 2017
-# last modified 14 May 2017
+# last modified 18 November 2017
 {
     # Setting up the parameters
     width <- as.integer(width) 
-    parsed <- .parseParam(param, bin=TRUE, width=width)
-    chrs <- parsed$chrs
-    frag.by.chr <- parsed$frag.by.chr
-    discard <- parsed$discard
-    cap <- parsed$cap
-    bwidth <- parsed$bwidth
-    bin.region <- parsed$bin.region
-    bin.id <- parsed$bin.id
-    bin.by.chr <- parsed$bin.by.chr
-
-    # Checking the inputs.
     first.chr <- as.character(seqnames(first.region))
     second.chr <- as.character(seqnames(second.region))
-    if (length(first.chr)!=1L) { stop("'first.region' should be of length 1") }
-    if (length(second.chr)!=1L) { stop("'second.region' should be of length 1") }
+    if (length(first.chr)!=1L) { 
+        stop("'first.region' should be of length 1") 
+    }
+    if (length(second.chr)!=1L) { 
+        stop("'second.region' should be of length 1") 
+    }
+
+    # Constructing bins across the genome.
+    is.dnase <- .isDNaseC(param)
+    if (is.dnase) { 
+        retainer <- c("anchor1.pos", "anchor2.pos", "anchor1.len", "anchor2.len")
+        bin.out <- .createBins(param, width, restricted=FALSE)
+    } else {
+        retainer <- c("anchor1.id", "anchor2.id")
+        bin.out <- .assignBins(param, width, restricted=FALSE)
+    }
+    bin.region <- bin.out$region
+    bin.id <- bin.out$id
+    bin.by.chr <- .splitByChr(bin.region)
+
+    # Identifying the boxes that lie within our ranges of interest. We give it some leeway
+    # to ensure that edges of the plot are retained.
+    chrs <- seqlevels(bin.region)
     if (!(first.chr %in% chrs) || !(second.chr %in% chrs)) { 
         stop("anchor chromosome names not in cut site list") 
     }
-    
-    # Identifying the boxes that lie within our ranges of interest. We give it some leeway
-    # to ensure that edges of the plot are retained.
     keep.frag.first <- keep.frag.second <- suppressWarnings(overlapsAny(bin.region, first.region)[bin.id])
     if (suppressWarnings(first.region!=second.region)) { 
         keep.frag.second <- suppressWarnings(overlapsAny(bin.region, second.region)[bin.id])
     }
 
     # Pulling out the read pair indices from each file, and checking whether chromosome names are flipped around.
-    all.dex <- .loadIndices(file, chrs)
+    param <- reform(param, restrict=cbind(first.chr, second.chr))
+    loadfuns <- preloader(file, param=param, retain=retainer)
     flipped <- FALSE
-    if (!is.null(all.dex[[first.chr]][[second.chr]])) {
-        current <- .baseHiCParser(TRUE, file, first.chr, second.chr, 
-            chr.limits=frag.by.chr, discard=discard, cap=cap, width=bwidth)[[1]]
+
+    if (!is.null(loadfuns[[first.chr]][[second.chr]])) {
+        current <- loadfuns[[first.chr]][[second.chr]][[1]]()
         filter.a <- keep.frag.first
         filter.t <- keep.frag.second
         t.start <- bin.by.chr$first[[second.chr]]
         t.end <- bin.by.chr$last[[second.chr]]
 
-    } else if (!is.null(all.dex[[second.chr]][[first.chr]])) { 
-        current <- .baseHiCParser(TRUE, file, second.chr, first.chr, 
-            chr.limits=frag.by.chr, discard=discard, cap=cap, width=bwidth)[[1]]
+    } else if (!is.null(loadfuns[[second.chr]][[first.chr]])) { 
+        current <- loadfuns[[second.chr]][[first.chr]][[1]]()
         filter.a <- keep.frag.second
         filter.t <- keep.frag.first
         t.start <- bin.by.chr$first[[first.chr]]
@@ -59,7 +66,7 @@ extractPatch <- function(file, param, first.region, second.region=first.region, 
         filter.a <- filter.t <- logical(0) 
         t.start <- bin.by.chr$first[[second.chr]]
         t.end <- bin.by.chr$last[[second.chr]]
-        current<-data.frame(anchor1.id=integer(0), anchor2.id=integer(0)) 
+        current <- data.frame(anchor1.id=integer(0), anchor2.id=integer(0)) 
     }
 
     # Getting the read pairs around the area of interest.
