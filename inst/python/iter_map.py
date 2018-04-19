abeb1f55a2003d69d65b20e772cdb6f05ea46a68
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-04-19 17:46:05 +0100

    Converted iterative alignment script to python3, Bio.SeqIO.

--- inst/python/iter_map.py
+++ inst/python/iter_map.py
@@ -3,7 +3,6 @@
 ##################################################################################################
 # iter_map.py: written by Aaron Lun
 # written 5 June, 2014
-# last modified 7 August, 2014
 
 import argparse
 parser = argparse.ArgumentParser(description="""Perform alignment of Hi-C read pairs with iterative mapping using Bowtie2, a la Imakaev et al.""",
@@ -16,15 +15,16 @@ parser.add_argument("-o", type=str, dest="output", help="path to output BAM file
 parser.add_argument("--cmd", type=str, dest="cmd", help="path to/command for bowtie2 executable, with or without flags", default="bowtie2")
 args = parser.parse_args()
 
+# Setting up imports
 import os
-from subprocess import call
+from subprocess import Popen, PIPE
+
+from Bio import SeqIO
 import pysam
 import tempfile
 tmpdir=tempfile.mkdtemp(dir=".")
 
-outdir=args.output
-bottom=25
-step=5
+import gzip
 
 # Processing the bowtie2 flag.
 import re
@@ -32,95 +32,116 @@ bwt2cmd = re.split("\s+", args.cmd.strip()) + ["-x", args.genome, "--phred"+str(
 
 ####################################################################################################
 
+is_gz=re.compile("\\.gz$")
+def open_handle(fname, write=False):
+    """A function that returns a Gzip file handle or a normal file handle, depending on the file name."""
+    if (is_gz.search(fname)):
+        if write:
+            mode='wb'
+        else:
+            mode='rb'
+
+        return gzip.open(fname, mode)
+    else:
+        if write:
+            mode='w'
+        else:
+            mode='rU'
+
+        return open(fname, mode)
+
+####################################################################################################
+
 allnames=[]
 dumpf=open(os.devnull, "w")
+bottom=25
+step=5
+
 for x, curf in enumerate([args.fq1, args.fq2]):
-	# Running through and checking for read length uniformity (also get rid of the newline).
-	counter=0
-	toplen=0
-	for line in open(curf, 'rU'):
-		counter+=1
-		if counter==2:
-			toplen = len(line)
-		elif counter%4==2 and toplen!=len(line):
-			raise ValueError, "varying read lengths are not supported"
-	toplen-=1 
+    # Running through and checking for read length uniformity (also get rid of the newline).
+    counter=0
+    toplen=0
 
-	# Resetting flags for first and second.
-	if x==0:
-		add=0x41
-	else:
-		add=0x81
+    with open_handle(curf) as in_handle: 
+        for record in SeqIO.parse(in_handle, "fastq"):
+            if not counter:
+                toplen=len(record)
+            elif toplen!=len(record):
+                raise ValueError("varying read lengths are not supported")
 
-	# Defining the assorted files.
-	fname, fext = os.path.splitext(os.path.basename(curf))
-	currentfq=os.path.join(tmpdir, "temp.fq")
-	os.link(curf, currentfq)
-	newfq=os.path.join(tmpdir, "next.fq")
-	newsam=os.path.join(tmpdir, "temp.sam")
-	allbam=os.path.join(tmpdir, fname+".bam")
+    # Resetting flags for first and second.
+    if x==0:
+        add=0x41
+    else:
+        add=0x81
 
-	starting=True
-	for newlen in xrange(bottom, toplen+step, step):
-		thrown_away=max(toplen-newlen, 0)
-		if call(bwt2cmd+["--reorder", "--very-sensitive", "-U", currentfq, "-S", newsam, "--trim3", str(thrown_away)], stdout=dumpf, stderr=dumpf):
-			raise SystemError, "bowtie2 failed for iterative alignment"
+    # Defining the assorted files.
+    fname, fext = os.path.splitext(os.path.basename(curf))
+    currentfq=os.path.join(tmpdir, "temp.fq")
+    newfq=os.path.join(tmpdir, "next.fq")
+    newsam=os.path.join(tmpdir, "temp.sam")
+    allbam=os.path.join(tmpdir, fname+".bam")
 
-		fqout=open(newfq, mode="w")
-		fqin=open(currentfq, mode="rU")
-		samin=pysam.Samfile(newsam, 'r')
-		if starting:
-			samout=pysam.Samfile(allbam, "wb", template=samin)
-			starting=False
+    starting=True
+    for newlen in xrange(bottom, toplen+step+1, step):
+        thrown_away=max(toplen-newlen, 0)
 
-		# Gradually extending the read from the 3' end. We compare the old
-		# FASTQ with the old sam, see which ones aligned and which ones didn't.
-		for curalign in samin:
-			try:
-				curname=fqin.readline()
-				curseq=fqin.readline()	
-				curother=fqin.readline()
-				curqual=fqin.readline()
-			except StopIteration:
-				raise ValueError, "ran out of reads in the FASTQ file"
-			if re.split("\s+", curname.strip())[0][1:]!=curalign.qname:
-				raise ValueError, "mismatch between read names in FASTQ and BAM file"
-			
-			# Keeping only mapped alignments that have positive MAPQ and no XS tag.	Full length
-			# reads also require uniqueness, otherwise they are added as empties.
-			if curalign.flag&0x4==0 and 'XS' not in dict(curalign.tags) and curalign.mapq > 0:
-				if thrown_away:
-					if curalign.flag&0x10:
-						curalign.cigar = [(5, thrown_away)] + curalign.cigar
-					else:
-						curalign.cigar += [(5, thrown_away)]
-				curalign.flag += add
-				samout.write(curalign)
-			elif thrown_away:
-				fqout.write(curname)
-				fqout.write(curseq)
-				fqout.write(curother)
-				fqout.write(curqual)
-			else:
-				curalign.flag = 0x4 + add
-				curalign.mapq = 0
-				samout.write(curalign)
-		
-		# Shuffling some files around.	
-		fqout.close()
-		fqin.close()
-		samin.close()
-		os.remove(currentfq)
-		os.rename(newfq, currentfq)
+        # Aligning reads.
+        if starting:
+            read_src=curf
+        else:
+            read_src=currentfq
 
-	# Cleaning up.
-	samout.close()
-	os.remove(currentfq)
-	os.remove(newsam)
-	bsorted=os.path.join(tmpdir, "sorted")
-	pysam.sort("-n", allbam, bsorted)
-	os.rename(bsorted+".bam", allbam)
-	allnames.append(allbam)
+        map_proc=Popen(bwt2cmd+["--reorder", "--very-sensitive", "-U", read_src, "-S", newsam, "--trim3", str(thrown_away)], stdout=dumpf, stderr=PIPE)
+        if map_proc.wait():
+            map_out, map_err=map_proc.communicate()
+            raise SystemError("bowtie2 failed for iterative alignment\n"+map_err)
+
+        # Running through the reads and figuring out which ones aligned (or did not).
+        with open_handle(read_src) as fqin, \
+                open(newfq, mode="w") as fqout, \
+                pysam.Samfile(newsam, 'r') as samin:
+
+            inparse=SeqIO.parse(fqin, "fastq")
+            if starting:
+                samout=pysam.Samfile(allbam, "wb", template=samin)
+                starting=False
+
+            # Gradually extending the read from the 3' end. We compare the old
+            # FASTQ with the old sam, see which ones aligned and which ones didn't.
+            for curalign in samin:
+                record=inparse.next()
+                if record.id!=curalign.qname:
+                    raise ValueError("mismatch between read names in FASTQ and BAM file")
+                
+                # Keeping only mapped alignments that have positive MAPQ and no XS tag.    Full length
+                # reads also require uniqueness, otherwise they are added as empties.
+                if curalign.flag&0x4==0 and 'XS' not in dict(curalign.tags) and curalign.mapq > 0:
+                    if thrown_away:
+                        if curalign.flag&0x10:
+                            curalign.cigar = [(5, thrown_away)] + curalign.cigar
+                        else:
+                            curalign.cigar += [(5, thrown_away)]
+                    curalign.flag += add
+                    samout.write(curalign)
+                elif thrown_away:
+                    SeqIO.write(record, fqout, "fastq")
+                else:
+                    curalign.flag = 0x4 + add
+                    curalign.mapq = 0
+                    samout.write(curalign)
+            
+        # Shuffling some files around.    
+        os.rename(newfq, currentfq)
+
+    # Cleaning up.
+    samout.close()
+    os.remove(currentfq)
+    os.remove(newsam)
+    bsorted=os.path.join(tmpdir, "sorted.bam")
+    pysam.sort("-n", allbam, "-o", bsorted)
+    os.rename(bsorted+".bam", allbam)
+    allnames.append(allbam)
 
 ####################################################################################################
 # Running through and merging the same BAM files together.
@@ -128,7 +149,7 @@ for x, curf in enumerate([args.fq1, args.fq2]):
 cmd=["-n", "-f", args.output]+allnames
 pysam.merge(*cmd)
 for x in allnames:
-	os.remove(x)
+    os.remove(x)
 
 # Mopping up.
 import shutil
