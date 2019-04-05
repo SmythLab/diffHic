12c398331da8a98dcf0269858ab26f7a71358b84
Aaron Lun <LTLA@users.noreply.github.com>
2019-04-05 11:01:52 -0700

    Delete unnecessary python test script.

--- inst/python/blah.py
+++
@@ -1,8 +0,0 @@
-from Bio import SeqIO
-import gzip
-
-with open("tmpxcmM8P/splitted_temp.fastq", "rU") as splitted:
-    original=SeqIO.parse(gzip.open("SRR027957_2.fastq.gz", "rb"), "fastq")
-    for record in SeqIO.parse(splitted, "fastq"):
-        orecord=original.next()
-
